53d72b56d280bebb570feb9e7c670075b5ccb59d
Winnie Kelley <191692065+wk642@users.noreply.github.com>
2026-07-05 23:29:07 -0500

    Adding comments of what I need to do

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,16 @@
 
 export default function StudentWork() {
   //add variables here
+  // A variable for name
+  // A variable for age
+  // A list of hobbies using an array
   return (
     <div>
       {/* add JSX here */}
       <p> Student output will go here </p>
+      {/* A heading about me */}
+      {/* A short paragraph introducing myself */}
+      {/* Map out my hobbies or interests so they all appear on the page in a list. */}
     </div>
   );
 }
