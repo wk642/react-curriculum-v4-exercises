f54ba9efe5b85abbd28a397caa117f7885f8d5dd
Winnie Kelley <191692065+wk642@users.noreply.github.com>
2026-07-14 20:56:02 -0500

    Added SnackFooter.jsx with footer

---
+++ src/exercises/lesson-02/SnackFooter.jsx
@@ -0,0 +1,8 @@
+// - Default export a small component that returns a fun footer message.
+export default function SnackFooter() {
+  return (
+    <>
+      <p>Winnie Kelley's Snacks Rankings</p>
+    </>
+  );
+}
