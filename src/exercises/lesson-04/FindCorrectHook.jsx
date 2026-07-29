cd5c62718ce0177b61fe6e9349d7b01b2cef26a6
Winnie Kelley <191692065+wk642@users.noreply.github.com>
2026-07-29 06:36:03 -0500

    Finished #2 FindCorrectHook

--- src/exercises/lesson-04/FindCorrectHook.jsx
+++ src/exercises/lesson-04/FindCorrectHook.jsx
@@ -1,10 +1,14 @@
 // TOPIC: Choose the correct tool: useRef vs useState
 // TASK: Make sure it updates the text *without* triggering a re-render
+import { useState } from 'react';
+
 export default function FindCorrectHook() {
-  let clickCount = 0; // ← incorrect implementation
+  // let clickCount = 0; // ← incorrect implementation
+  const [clickCount, setClickCount] = useState(0);
 
   function handleClick() {
-    clickCount++;
+    // clickCount++;
+    setClickCount((count) => count + 1);
   }
 
   return (
