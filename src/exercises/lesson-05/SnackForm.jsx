223cf235e07bfc7b8f651dc775cde146fe11941f
Winnie Kelley <191692065+wk642@users.noreply.github.com>
2026-08-04 22:13:56 -0500

    Finished Pt 2 Step 3

--- src/exercises/lesson-05/SnackForm.jsx
+++ src/exercises/lesson-05/SnackForm.jsx
@@ -77,6 +77,7 @@ export default function SnackForm({
           className={styles['field-input']}
           placeholder="Enter snack name"
         />
+        {getNameError() && <div className={styles.error}>{getNameError()}</div>}
       </div>
 
       <div className={styles['field-container']}>
@@ -92,6 +93,9 @@ export default function SnackForm({
           className={styles['field-input']}
           placeholder="Rate 1-5"
         />
+        {getRatingError() && (
+          <div className={styles.error}>{getRatingError()}</div>
+        )}
       </div>
 
       <div className={styles['button-container']}>
